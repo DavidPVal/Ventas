a85a2f3c4b30f2c9d089c7dcbe337d97c19245f7
DavidPVal <55401492+DavidPVal@users.noreply.github.com>
2020-04-20 09:50:48 +0200

    Controlador Productos

---
+++ Ventas.Backend/Controllers/ProductoController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ventas.Backend.Models;
+using Ventas.Common.Models;
+
+namespace Ventas.Backend.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private LocalDataContext db = new LocalDataContext();
+
+        // GET: Producto
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Productoes.ToListAsync());
+        }
+
+        // GET: Producto/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = await db.Productoes.FindAsync(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Producto/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "IDProducto,Descripcion,Precio,IsAvailable,FechaPublicacion")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Productoes.Add(producto);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(producto);
+        }
+
+        // GET: Producto/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = await db.Productoes.FindAsync(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "IDProducto,Descripcion,Precio,IsAvailable,FechaPublicacion")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(producto).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(producto);
+        }
+
+        // GET: Producto/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Producto producto = await db.Productoes.FindAsync(id);
+            if (producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(producto);
+        }
+
+        // POST: Producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Producto producto = await db.Productoes.FindAsync(id);
+            db.Productoes.Remove(producto);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ventas.Common/Models/Producto.cs
@@ -0,0 +1,20 @@
+﻿namespace Ventas.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Producto
+    {
+        [Key]
+        public int IDProducto { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public DateTime FechaPublicacion { get; set; }
+    }
+}
